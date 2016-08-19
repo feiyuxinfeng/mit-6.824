9728aedaa0230f641eb17683ea58b0523958818c
YuYang <yy2012cn@gmail.com>
2016-08-19 20:06:32 +0800

    trivial

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -24,5 +24,41 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	debug("in schedule")
+	args := make([]*DoTaskArgs, 0)
+	for i := 0; i < ntasks; i++ {
+		arg := &DoTaskArgs{
+			JobName:       mr.jobName,
+			File:          mr.files[i],
+			Phase:         phase,
+			TaskNumber:    i,
+			NumOtherPhase: nios,
+		}
+		args = append(args, arg)
+	}
+
+	finishedChan := make(chan string)
+	// mr.Lock()
+	// for i := 0; i < len(mr.workers); i++ {
+	// 	finishedChan <- mr.workers[i]
+	// }
+	// mr.Unlock()
+	for _, arg := range args {
+		// get a worker
+
+		var wk string
+		select {
+		case wk = <-mr.registerChannel:
+		case wk = <-finishedChan:
+
+		}
+		// wk := <-finishedChan
+
+		go func(wk string, arg *DoTaskArgs) {
+			call(wk, "Worker.DoTask", arg, new(struct{}))
+			finishedChan <- wk
+		}(wk, arg)
+
+	}
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
