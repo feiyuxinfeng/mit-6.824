670ce1df26113fa406eaeb3295f3a318409f804c
YuYang <yy2012cn@gmail.com>
2016-11-13 14:51:03 +0800

    better implemention of test script

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -16,8 +16,43 @@ ppdir="$(dirname "$pdir")"
 export GOPATH=$ppdir
 cd $cur_dir
 
+tmpfile=$(mktemp /tmp/abc-script.XXXXXX)
+
+test_name=""
 num_loop=10
 
+if [ "$#" -eq 1 ]; then
+    re='^[0-9]+$'
+    if ! [[ $1 =~ $re ]] ; then
+        # echo "error: Not a number" >&2; exit 1
+        test_name=$1
+    else
+        num_loop=$1
+    fi
+elif [ "$#" -eq 2 ]; then
+    test_name=$1
+    num_loop=$2
+fi
+
+echo "test name: $test_name"
+echo "loop $num_loop times"
+
 for ((i=0;i<$num_loop;i++)); do
-    go test -run $1
+    echo "$i +++++"
+    if [ -z "$test_name" ]; then
+        go test | tee -a $tmpfile
+    else
+        go test -run $test_name | tee -a $tmpfile
+    fi
 done
+
+pass=`grep PASS $tmpfile | wc -l`
+failed=`grep FAIL $tmpfile | wc -l`
+echo
+echo "========= result ====================="
+echo
+echo "sucess: $pass"
+echo "fail: $failed"
+echo
+echo "======== end ======================="
+rm "$tmpfile"
