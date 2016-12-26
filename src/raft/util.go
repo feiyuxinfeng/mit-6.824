47842efc32446f2d90001d93874d7fbb5fb7402f
YuYang <yy2012cn@gmail.com>
2016-12-26 21:36:45 +0800

    better dprintf [skip ci]

--- src/raft/util.go
+++ src/raft/util.go
@@ -8,14 +8,14 @@ import (
 )
 
 // Debugging
-const Debug = 0
+const Debug = 1
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
-		log.Printf(format, a...)
-	}
-	return
-}
+// func DPrintf(format string, a ...interface{}) (n int, err error) {
+// 	if Debug > 0 {
+// 		log.Printf(format, a...)
+// 	}
+// 	return
+// }
 
 func getGID() uint64 {
 	b := make([]byte, 64)
@@ -24,4 +24,31 @@ func getGID() uint64 {
 	b = b[:bytes.IndexByte(b, ' ')]
 	n, _ := strconv.ParseUint(string(b), 10, 64)
 	return n
+}
+
+func dprintf(level int, format string, a ...interface{}) (n int, err error) {
+	if level >= Debug {
+		log.Printf(format, a...)
+	}
+	return
+}
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	dprintf(0, format, a...)
+	return
+}
+
+func D1Printf(format string, a ...interface{}) (n int, err error) {
+	dprintf(1, format, a...)
+	return
+}
+
+func D2Printf(format string, a ...interface{}) (n int, err error) {
+	dprintf(2, format, a...)
+	return
+}
+
+func D3Printf(format string, a ...interface{}) (n int, err error) {
+	dprintf(3, format, a...)
+	return
 }
