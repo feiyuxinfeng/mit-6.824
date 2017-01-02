25ed65c8545dddc8ca2714fb3fd65c9a158cd78a
YuYang <yy2012cn@gmail.com>
2017-01-02 23:14:51 +0800

    Lab 3 Part A I

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -1,13 +1,18 @@
 package raftkv
 
-import "labrpc"
-import "crypto/rand"
-import "math/big"
-
+import (
+	"crypto/rand"
+	"labrpc"
+	"math/big"
+	mrand "math/rand"
+	"time"
+)
 
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+	leader  int
+	seenXid int64
 }
 
 func nrand() int64 {
@@ -21,7 +26,59 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.leader = 0
 	return ck
+}
+
+func (ck *Clerk) chooseLeader(prev int) int {
+	if prev < 0 {
+		return ck.leader
+	}
+	leader := prev
+	for leader == prev {
+		leader = mrand.Intn(len(ck.servers))
+	}
+	return leader
+}
+
+func (ck *Clerk) sendGet(server int, args *GetArgs, reply *GetReply, timeoutMs int64) bool {
+	if timeoutMs == 0 {
+		ok := ck.servers[server].Call("RaftKV.Get", args, reply)
+		return ok
+	} else {
+		retChan := make(chan bool, 1)
+		go func(reply *GetReply, retChan chan bool) {
+			ok := ck.servers[server].Call("RaftKV.Get", args, reply)
+			retChan <- ok
+		}(reply, retChan)
+
+		select {
+		case ok := <-retChan:
+			return ok
+		case <-time.After(time.Duration(timeoutMs) * time.Millisecond):
+			return false
+		}
+	}
+}
+
+func (ck *Clerk) sendPutAppend(server int, args *PutAppendArgs, reply *PutAppendReply, timeoutMs int64) bool {
+	if timeoutMs == 0 {
+		ok := ck.servers[server].Call("RaftKV.PutAppend", args, reply)
+		return ok
+	} else {
+		retChan := make(chan bool, 1)
+		go func(reply *PutAppendReply, retChan chan bool) {
+			ok := ck.servers[server].Call("RaftKV.PutAppend", args, reply)
+			retChan <- ok
+		}(reply, retChan)
+
+		select {
+		case ok := <-retChan:
+			return ok
+		case <-time.After(time.Duration(timeoutMs) * time.Millisecond):
+			return false
+		}
+	}
 }
 
 //
@@ -39,7 +96,30 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
-	return ""
+	args := GetArgs{
+		Key: key,
+		Xid: nrand(),
+	}
+	leader := -1
+	for {
+		leader = ck.chooseLeader(leader)
+		reply := GetReply{}
+		ret := ck.sendGet(leader, &args, &reply, 0)
+		if ret == true {
+			if reply.WrongLeader {
+				DPrintf("Get(Xid: %v): %v", args.Xid, reply.Err)
+				continue
+			}
+			if reply.Err != "" {
+				DPrintf("Get(Xid: %v): %v", args.Xid, reply.Err)
+				continue
+			}
+			DPrintf("Get(Xid: %v) success.", args.Xid)
+			return reply.Value
+		} else {
+			DPrintf("Get(xid: %v) RPC fails", args.Xid)
+		}
+	}
 }
 
 //
@@ -54,11 +134,40 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	args := PutAppendArgs{
+		Key:   key,
+		Value: value,
+		Op:    op,
+		Xid:   nrand(),
+	}
+	leader := -1
+	for {
+		leader = ck.chooseLeader(leader)
+		reply := PutAppendReply{}
+		ret := ck.sendPutAppend(leader, &args, &reply, 0)
+		if ret == true {
+			if reply.WrongLeader {
+				// DPrintf("%v(Xid: %v): %v", op, args.Xid, reply.Err)
+				continue
+			}
+			if reply.Err != "" {
+				DPrintf("%v(Xid: %v): %v", op, args.Xid, reply.Err)
+				continue
+			}
+			ck.leader = leader
+			DPrintf("%v(Xid: %v) success.", op, args.Xid)
+			return
+		} else {
+			DPrintf("%v(xid: %v) RPC fails", op, args.Xid)
+		}
+	}
+
 }
 
 func (ck *Clerk) Put(key string, value string) {
 	ck.PutAppend(key, value, "Put")
 }
+
 func (ck *Clerk) Append(key string, value string) {
 	ck.PutAppend(key, value, "Append")
 }
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -2,26 +2,42 @@ package raftkv
 
 import (
 	"encoding/gob"
+	"fmt"
 	"labrpc"
 	"log"
 	"raft"
 	"sync"
 )
 
-const Debug = 0
+type OpType int
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
-		log.Printf(format, a...)
+const (
+	GET = OpType(iota)
+	PUT
+	APPEND
+)
+
+func (ty OpType) String() string {
+	switch ty {
+	case GET:
+		return "GET"
+	case PUT:
+		return "PUT"
+	case APPEND:
+		return "APPEND"
+	default:
+		return "Unkown"
 	}
-	return
 }
-
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Type  OpType
+	Key   string
+	Value string
+	Xid   int64
 }
 
 type RaftKV struct {
@@ -33,15 +49,146 @@ type RaftKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	state   map[string]string
+	chanMap map[int64]chan Op
+	xidMap  map[int64]Op
 }
 
+func (kv *RaftKV) processApplyLog() {
+	for m := range kv.applyCh {
+		kv.mu.Lock()
+		if op, ok := (m.Command).(Op); ok {
+			switch op.Type {
+			case GET:
+				op.Value = kv.state[op.Key]
+			case PUT:
+				kv.state[op.Key] = op.Value
+			case APPEND:
+				if _, exists := kv.state[op.Key]; exists {
+					kv.state[op.Key] = kv.state[op.Key] + op.Value
+				} else {
+					kv.state[op.Key] = op.Value
+				}
+			}
+
+			if ch, ok := kv.chanMap[op.Xid]; ok {
+				delete(kv.chanMap, op.Xid)
+
+				DPrintf("Found Chan: server %v receive apply log %v, xid: %v, op: %v", kv.me, m.Index, op.Xid, op.Type.String())
+				go func() {
+					ch <- op
+				}()
+			} else {
+				DPrintf("No Chan: server %v receive apply log %v, xid: %v, op: %v", kv.me, m.Index, op.Xid, op.Type.String())
+			}
+		}
+
+		kv.mu.Unlock()
+	}
+}
 
 func (kv *RaftKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	op := Op{
+		Type: GET,
+		Xid:  args.Xid,
+		Key:  args.Key,
+	}
+
+	kv.mu.Lock()
+	resultCh := make(chan Op, 1)
+
+	if _, exists := kv.chanMap[args.Xid]; !exists {
+		kv.chanMap[args.Xid] = resultCh
+	} else {
+		log.Fatalf("chan Op for Xid %v exists", args.Xid)
+	}
+	kv.mu.Unlock()
+
+	index, _, isLeader := kv.rf.Start(op)
+
+	DPrintf("server receive %v Get RPC xid: %v, Index: %v, isLeader: %v", kv.me, args.Xid, index, isLeader)
+
+	reply.WrongLeader = !isLeader
+
+	defer func() {
+		if reply.Err != "" {
+			kv.mu.Lock()
+			delete(kv.chanMap, args.Xid)
+			kv.mu.Unlock()
+		}
+	}()
+
+	if isLeader == false {
+		reply.Err = Err(fmt.Sprintf("server %v is not a leader", kv.me))
+		return
+	}
+
+	resultOp := <-resultCh
+
+	if resultOp.Xid != args.Xid {
+		reply.Err = "conflit xid"
+		reply.Value = ""
+		return
+	}
+	reply.Err = ""
+	reply.Value = resultOp.Value
+	return
 }
 
 func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	op := Op{
+		Key:   args.Key,
+		Value: args.Value,
+		Xid:   args.Xid,
+	}
+	if args.Op == "Put" {
+		op.Type = PUT
+	} else if args.Op == "Append" {
+		op.Type = APPEND
+	} else {
+		log.Fatalf("Unkown rpc type %v", args.Op)
+		return
+	}
+
+	kv.mu.Lock()
+
+	resultCh := make(chan Op, 1)
+
+	if _, exists := kv.chanMap[args.Xid]; !exists {
+		kv.chanMap[args.Xid] = resultCh
+	} else {
+		log.Fatalf("chan Op for Xid %v exists", args.Xid)
+	}
+	kv.mu.Unlock()
+
+	index, _, isLeader := kv.rf.Start(op)
+
+	DPrintf("server %v receive %v RPC xid: %v, Index: %v, isLeader: %v", args.Op, args.Xid, index, isLeader)
+	reply.WrongLeader = !isLeader
+
+	defer func() {
+		if reply.Err != "" {
+			kv.mu.Lock()
+			delete(kv.chanMap, args.Xid)
+			kv.mu.Unlock()
+		}
+	}()
+
+	if isLeader == false {
+		reply.Err = Err(fmt.Sprintf("server %v is not a leader", kv.me))
+		return
+	}
+
+	resultOp := <-resultCh
+	if resultOp.Xid != args.Xid {
+		reply.Err = "conflit xid"
+		return
+	}
+
+	reply.Err = ""
+	return
 }
 
 //
@@ -78,10 +225,14 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// Your initialization code here.
+	kv.state = make(map[string]string)
+	kv.chanMap = make(map[int64]chan Op)
+	kv.xidMap = make(map[int64]Op)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
+	go kv.processApplyLog()
 
 	return kv
 }
